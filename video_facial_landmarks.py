e418817c8ea17745c8a5d142902e054d9e076270
limjustin <limjyjustin@gmail.com>
2020-11-23 21:09:00 +0900

    Complete linking code

--- video_facial_landmarks.py
+++ video_facial_landmarks.py
@@ -14,7 +14,6 @@ import cv2
 
 import numpy as np
 
-##############################################################################################################
 #광학 계산 위한 것들
 feature_params = dict(maxCorners=100,
                       qualityLevel=0.3,
@@ -26,6 +25,9 @@ lk_params = dict(winSize=(15, 15),
                  criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
 cap = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('./hello.avi', fourcc, 20.0, (1024, 768))  # 창 크기 조절
+
 
 def get_coords(p1):
    try:
@@ -33,7 +35,6 @@ def get_coords(p1):
    except:
       return int(p1[0][0]), int(p1[0][1])
 
-##############################################################################################################
 
 
 
@@ -45,45 +46,24 @@ predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat") # pred
 
 # initialize the video stream and allow the cammera sensor to warmup
 print("[INFO] camera sensor warming up...")
-# vs = VideoStream(0).start()  # Modify : VideoStream(0).start() -> cv2.VideoCapture(0)
-# print("vs is ", vs)
-# cap = cv2.VideoCapture(0)  # Add this code
-# print("cap is ", cap)
 cap = cv2.VideoCapture(0)
-
 time.sleep(2.0)
 
-# #################################################################
-# # Copy vs.read() before cap.read()
-# temp = vs.read()
-#
-# # VideoStream과 VideoCapture의 차이
-# ret, frame_cap = cap.read()  # Modify : cap -> vs && frame -> frame_cap
-#                              # VideoCapture와 cap.read()는 한 세트인가?
-#                              # read()를 먼저 따오니까 오류가 나는 것인가? -> 가져온 것이니까...? 그냥 명시 안하고 가져왔으니까 다 가져와서 뭐가 없네...
-#                              # 긁어올 것들이 없어진거지 비디오 체크하는 것은 똑같으니까
-#                              # 값을 복사해서 가져오는 것은 어떻게 생각해?
-# frame_gray = cv2.cvtColor(frame_cap, cv2.COLOR_BGR2GRAY)
-#
-# #################################################################
+# define movement threshodls
+max_head_movement = 20
+movement_threshold = 50
+gesture_threshold = 175
 
-print("1")
-# loop over the frames from the video stream
+gesture = False
+x_movement = 0
+y_movement = 0
+gesture_show = 60  # number of frames a gesture is shown
+
+stop_cnt = 0
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+
 while True:
-    # grab the frame from the threaded video stream, resize it to
-    # have a maximum width of 400 pixels, and convert it to
-    # grayscale
-
-    # #################################################################
-	# # vs.read()는 실시간으로 계속 화면 사진을 read 해오는 역할이네
-	# # 초반에 긁어올 것이 없다는 것만 해결하면 가능할텐데...
-	# # 따라서 얘는 그냥 vs.read()로 써야함 안그러면 실시간으로 화면 캡쳐 안됨
-    # frame = vs.read()  # 시작된 비디오를 읽어와라 && Modify : vs.read() -> temp
-    # print("frame is ", frame)  # 이게 None로 비었네...
-    # frame = imutils.resize(frame, width=400)  # 화면 크기 늘릴 수 있음
-    # print("Second frame is ", frame)
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # #################################################################
 
     ret, old_frame = cap.read()
     old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)  # from color to black and white
@@ -91,37 +71,89 @@ while True:
     ret, frame = cap.read()
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # 얘는 밖으로 빼도 되겠다
 
-    # # detect faces in the grayscale frame
-    # rects = detector(gray, 0)
-
-
-    # loop over the face detections
     for rect in rects:
-        # determine the facial landmarks for the face region, then
-        # convert the facial landmark (x, y)-coordinates to a NumPy
-        # array
         shape = predictor(frame_gray, rect)
         shape = face_utils.shape_to_np(shape)
-        # print(shape)
-        x, y = shape[30]  # 34는 너무 콧구멍           
+        x, y = shape[30]  # 34는 너무 콧구멍
 
         # 광학계산코드
         p0 = cv2.goodFeaturesToTrack(old_gray, mask=None, **feature_params)
 
-        # cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
-        # print(x, ",", y)
-
         # x랑 y를 numpy 배열을 이용하여 만들어보기
         face_center = x, y
         p0 = np.array([[face_center]], np.float32)  # 먼저 움직이는 얼굴 중심 좌표
-        # print("p0 is ", p0)
 
         p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
 
         cv2.circle(frame, get_coords(p0), 3, (0, 0, 255))
         # 빨간색이 나한테 붙어있는 점
+
         cv2.circle(frame, get_coords(p1), 3, (255, 0, 0), -1)
         # 파란색이 따라다니는 점
+
+        # get the xy coordinates for points p0 and p1
+        print("p0 is ", p0)
+        print("p1 is ", p1)
+        a, b = get_coords(p0), get_coords(p1)
+        print("a is ", a)
+        print("b is ", b)
+
+        if abs(a[0] - b[0]) > 3 or abs(a[1] - b[1]) > 3:  # 움직임 최소화하기
+            x_movement += abs(a[0] - b[0])
+            y_movement += abs(a[1] - b[1])
+
+        print("x_movement is ", x_movement)
+        print("y_movement is ", y_movement)
+
+        print("gesture_show is ", gesture_show)
+
+        text = 'x_movement: ' + str(x_movement)
+        if not gesture: cv2.putText(frame, text, (50, 50), font, 0.8, (0, 0, 255), 2)
+        text = 'y_movement: ' + str(y_movement)
+        if not gesture: cv2.putText(frame, text, (50, 100), font, 0.8, (0, 0, 255), 2)
+
+        # 도리도리는 150 / 끄덕끄덕은 30 정도 움직임
+
+        # 원래는 gesture_threshold 값을 가짐
+        # 반응이 아니어도 움직임이 허용되는 선의 범위 안에서 gesture_threshold를 정해야 함
+        if x_movement > gesture_threshold:
+            print(">>> Gesture is No, x_movement is ", x_movement)
+            gesture = 'No'
+        if y_movement > gesture_threshold:
+            print(">>> Gesture is Yes, y movement is ", y_movement)
+            gesture = 'Yes'
+
+        # 의문 상황은 대각선으로 움직이는 것을 포착하면 되는데
+        # 이 상황이 위에 상황과 겹쳐지면 어떻게 해야 하나를 지금 생각 중
+        # 좀 더 강력하게 구분될 수 있는 상황을 만들어야겠음
+        # 각 증분에 대한 기울기
+
+        if gesture and gesture_show > 0:
+            cv2.putText(frame, 'Gesture Detected: ' + gesture, (50, 50), font, 1.2, (0, 0, 255), 3)
+            gesture_show -= 1
+
+        if gesture_show == 0:
+            gesture = False
+            x_movement = 0
+            y_movement = 0  # 움직임이 없으면 0으로 초기화시키네
+            gesture_show = 60  # number of frames a gesture is shown
+
+        ##### 고개가 자연스럽게 움직일 수 있는 부분에서 허용되는 범위 #####
+        # 자연스럽게 고개가 움직이는 부분은 어떻게 할 것이냐
+        # 근데 이 count가 Gesture를 파악하는데 걸림돌이 되면 안된다
+        if stop_cnt > 50:  # 50 정도면 되는 것이냐
+            x_movement = 0
+            y_movement = 0
+            stop_cnt = 0
+
+        # print distance(get_coords(p0), get_coords(p1))
+        p0 = p1
+
+        # cv2.imshow('image', frame)
+        out.write(frame)
+        cv2.waitKey(1)
+
+        stop_cnt += 1
 
     # show the frame
     cv2.imshow("Frame", frame)
